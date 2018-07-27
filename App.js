4bf4a26795eb18313352b0f97bc6b3c2e4beee92
DEPRET <frederic.depret@ext.cdiscount.com>
2018-07-27 15:20:10 +0200

    native

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-  WebView,
-  View,
-  ScrollView,
-  StyleSheet,
-  Platform, TouchableHighlight,Text
+    WebView,
+    View,
+    ScrollView,
+    StyleSheet,
+    Platform, TouchableHighlight,Text
 } from 'react-native';
 
 const isAndroid= Platform.OS==='android'
@@ -15,7 +15,7 @@ export default class App extends Component {
     }
 
     lapsList(pos) {
-        const targeting =`pos:'${pos}'|requete:pneu|fullscreen:NON`;
+        const targeting =`pos:${pos}|requete:pneu|fullscreen:NON`;
         let target ='';
 
         targeting.split("|").forEach((data) => {
@@ -25,13 +25,13 @@ export default class App extends Component {
         return target;
     }
 
-render() {
+    render() {
 
-    var htmlContent = '<p><a href="http://jsdf.co">&hearts; nice job!</a></p>';
-
-    const jsCode = `
+        var htmlContent = '<p><a href="http://jsdf.co">&hearts; nice job!</a></p>';
+        // Cdiscount_App_Android
+        const jsCode = `
         googletag.cmd.push(function() {
-          var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/jardin-brico-animalerie/listeprod/pave-inpage', [300, 250] , "banner1");
+          var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/informatique/recherche/pave-inpage', [300, 250] , "banner1");
           adSlot1.addService(googletag.pubads());
           ${this.lapsList('pave-inpage')}
           googletag.pubads().set('page_url', 'http://www.cdiscount.com/');
@@ -40,47 +40,48 @@ render() {
       })`;
 
 
-    const jsCode2 = `
+        const jsCode2 = `
       googletag.cmd.push(function() {
-         var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/jardin-brico-animalerie/listeprod/banner-inpage',  [320, 50] , "banner2");
+         var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/informatique/recherche/banner-inpage',  [320, 50] , "banner2");
          adSlot1.addService(googletag.pubads());
           ${this.lapsList('banner-inpage')}
          googletag.pubads().set('page_url', 'http://www.cdiscount.com/');
          googletag.enableServices();
          googletag.display('banner2'); 
      })`;
-    const jsCode3 = `     
+        const jsCode3 = `      
          googletag.cmd.push(function() {
-            var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/jardin-brico-animalerie/listeprod/native-line', 'fluid', "banner3");
+            var adSlot1 = googletag.defineSlot('7190/Cdiscount_App_Android/informatique/recherche/native-line', ['fluid'], "banner3");
             adSlot1.addService(googletag.pubads());
            ${this.lapsList('native-line')}
             googletag.pubads().enableSingleRequest();
+            googletag.pubads().set('page_url', 'http://www.cdiscount.com/');
             googletag.enableServices();
             googletag.display('banner3'); 
         });`;
 
         return (
 
-          <View style={styles.container}>
-               <Text>Publicité</Text>
-               <WebView
-                   style={styles.webView}
-                   source={{uri:isAndroid?'file:///android_asset/widget/ad.html':'./widget/index.html', }}
-                   javaScriptEnabled={true}
-                   mixedContentMode="always"
-                   injectedJavaScript={jsCode}
-                   scrollEnabled={false}
-                   startInLoadingState={true}
-                   />
+            <View style={styles.container}>
+                <Text>Publicité</Text>
                 <WebView
-                   style={styles.webView2}
-                   source={{uri:isAndroid?'file:///android_asset/widget/ad.html':'./widget/index.html', }}
-                   javaScriptEnabled={true}
-                   mixedContentMode="always"
-                   injectedJavaScript={jsCode2}
-                   scrollEnabled={false}
-                   startInLoadingState={true}
-                  />
+                    style={styles.webView}
+                    source={{uri:isAndroid?'file:///android_asset/widget/ad.html':'./widget/index.html', }}
+                    javaScriptEnabled={true}
+                    mixedContentMode="always"
+                    injectedJavaScript={jsCode}
+                    scrollEnabled={false}
+                    startInLoadingState={true}
+                />
+                <WebView
+                    style={styles.webView2}
+                    source={{uri:isAndroid?'file:///android_asset/widget/ad.html':'./widget/index.html', }}
+                    javaScriptEnabled={true}
+                    mixedContentMode="always"
+                    injectedJavaScript={jsCode2}
+                    scrollEnabled={false}
+                    startInLoadingState={true}
+                />
                 <WebView
                     style={styles.webView3}
                     source= {{uri:isAndroid?'file:///android_asset/widget/ad.html':'./widget/index.html'}}
@@ -107,10 +108,10 @@ const styles = StyleSheet.create({
     },
     webView2: {
         backgroundColor: '#f0f',
-        flex: 2,
+        flex: 1,
     },
     webView3: {
-        backgroundColor: 'blue',
+        backgroundColor: '#fff',
         flex: 1,
     }
 })
